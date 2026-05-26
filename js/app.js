e23a788efec52a7a60e3389e1d6fb3fc589fc307
omowumi jeremiah <omowumijeremiah25@gmail.com>
2026-05-26 17:16:00 +0100

    getting closer

--- js/app.js
+++ js/app.js
@@ -29,3 +29,20 @@ function updateCartCount() {
   }
 }
 updateCartCount()
+
+const darkModeToogle = document.querySelector("#darkModeToggle")
+
+if(localStorage.getItem("darkMode") === "enabled"){
+  document.body.classList.add("dark-mode")
+}
+if(darkModeToogle){
+  darkModeToogle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode")
+    
+    if(document.body.classList.contains("dark-mode")){
+      localStorage.setItem("darkMode", "enabled")
+    } else {
+      localStorage.removeItem("darkMode")
+    }
+  })
+}
